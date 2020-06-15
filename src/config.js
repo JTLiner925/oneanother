543c944183c798e03851c8dd9bd8efdd83f888d7
JTLiner925 <[jtliner925@gmail.com]>
2020-06-15 08:23:07 -0500

    missed q on api request

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT:`https://api.esv.org/v3/passage/text/?john+1:1-10`,
+  API_ENDPOINT:`https://api.esv.org/v3/passage/text/?q=john+1:1-10`,
   API_KEY:'60a18c270d1e63e586a6c3cc744d3e237e77e557',
 }
