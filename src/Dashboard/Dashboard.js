e28fe78a47518abc883628b1071600bcd7d03a92
JTLiner925 <[jtliner925@gmail.com]>
2020-07-04 13:19:49 -0500

    userId

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -105,6 +105,7 @@ class Dashboard extends Component {
           users: users,
           groups: groups,
           events: events,
+          userId: users.user.id
         });
         // this.setUser(users[user].id)
       })
