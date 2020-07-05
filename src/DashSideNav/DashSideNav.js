e618995802eb61ec906d10d9517100ffcb850c0c
JTLiner925 <[jtliner925@gmail.com]>
2020-07-05 08:38:01 -0500

    parseInt

--- src/DashSideNav/DashSideNav.js
+++ src/DashSideNav/DashSideNav.js
@@ -68,7 +68,7 @@ export default class DashSideNav extends Component {
     if(this.props.users.length > 0){
       user_id = this.props.users.map((user) => {
         return (
-          user.id == userGroup
+          user.id == parseInt(userId)
         )
       })
     }
