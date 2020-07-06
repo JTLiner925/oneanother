6eedb821388f61ddd87198275bb9fb8fca42033f
JTLiner925 <[jtliner925@gmail.com]>
2020-07-05 22:14:26 -0500

    no-cors login

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
       headers: {
         "Content-Type": "application/json",
       },
-      mode: 'no-cors',
+      // mode: 'no-cors',
       method: "POST",
       body: JSON.stringify(formData),
     })
@@ -42,7 +42,7 @@ class App extends Component {
         "Content-Type": "application/json",
       },
 
-      // mode: 'no-cors',
+      mode: 'no-cors',
       method: "POST",
       body: JSON.stringify(formData),
     })
