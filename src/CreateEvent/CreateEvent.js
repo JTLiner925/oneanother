c049026d94bcfb731dba4bcc96b4213e0c927046
JTLiner925 <[jtliner925@gmail.com]>
2020-07-09 16:33:55 -0500

    passages[0].strip

--- src/CreateEvent/CreateEvent.js
+++ src/CreateEvent/CreateEvent.js
@@ -70,9 +70,9 @@ export default class CreateEvent extends Component {
 
     fetch(url, options)
       .then((res) => {
-       let passages = res.json()['passages']
+       let passages = res.json()
        if(passages){
-       return passages[0].strip()
+       return passages['passages'][0].strip()
        } else {
         throw new Error(
               'Please check Bible passage, write out in long form. i.e. "Matthew 28:18-20"'
