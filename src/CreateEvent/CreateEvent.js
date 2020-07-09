ced1073930521f8ac836d8ec177bf9f971f612a4
JTLiner925 <[jtliner925@gmail.com]>
2020-07-09 17:44:58 -0500

    res.passages

--- src/CreateEvent/CreateEvent.js
+++ src/CreateEvent/CreateEvent.js
@@ -73,7 +73,7 @@ export default class CreateEvent extends Component {
     fetch(url.href, options)
       .then((res) => {
         console.log(res);
-        if (!res.canonical === '' || !res.ok) {
+        if (res.passages === [] || !res.ok) {
           throw new Error(
             'Please check Bible passage, write out in long form. i.e. "Matthew 28:18-20"'
           );
