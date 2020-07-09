c34797668451ebd58e1e14a46016622672551971
JTLiner925 <[jtliner925@gmail.com]>
2020-07-09 17:00:22 -0500

    put passages in json

--- src/CreateEvent/CreateEvent.js
+++ src/CreateEvent/CreateEvent.js
@@ -17,12 +17,12 @@ export default class CreateEvent extends Component {
       error: null,
     });
   };
-  checkVerse = (passage) => {
-    this.setState({
-      passage,
-      error: null,
-    })
-  }
+  // checkVerse = (passage) => {
+  //   this.setState({
+  //     passage,
+  //     error: null,
+  //   })
+  // }
 
   componentDidMount() {
     fetch(`https://mighty-brook-70505.herokuapp.com/api/events`, {
@@ -70,7 +70,8 @@ export default class CreateEvent extends Component {
 
     fetch(url, options)
       .then((res) => {
-       let passages = res.json()['canonical']
+        console.log(res)
+       let passages = res.json(['passages'])
        console.log(passages)
        if(passages){
        return passages[0].strip()
@@ -88,7 +89,7 @@ export default class CreateEvent extends Component {
         // return res.json()
       })
       .then((res) => {
-        this.checkVerse(res)
+     
         this.props.onCreateEvent(this.state);
       })
       .catch((error) => {
